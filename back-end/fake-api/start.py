daa91e8282b2805ba76ff0762fdba6bd97ddf8e9
Douglas Saturnino <douglasaturnino@gmail.com>
2025-02-19 18:12:17 -0300

    feat: adiciona o faker no endpoit gerar_compra

--- back-end/fake-api/start.py
+++ back-end/fake-api/start.py
@@ -1,16 +1,30 @@
+import os
+import random
+
+import pandas as pd
+from faker import Faker
 from fastapi import FastAPI
 
 app = FastAPI()
+fake = Faker()
+
+file_name = os.path.join(os.path.dirname(__file__), "produto.csv")
+df = pd.read_csv(file_name)
+df["index"] = range(1, len(df) + 1)
+df.set_index("index", inplace=True)
 
 
 @app.get("/gerar_compra")
 async def gerar_compra():
+    index = random.randint(1, len(df) - 1)
+    tuple = df.iloc[index]
     return {
-        "cliente": "Nome",
-        "creditcard": "Tipo de cartão",
-        "ean": "Código de barras do produto",
-        "price": "Preço do produto",
+        "cliente": fake.name(),
+        "creditcard": fake.credit_card_provider(),
+        "product": tuple["produto"],
+        "ean": int(tuple["ean"]),
+        "price": round(float(tuple["price"]) * 1.2, 2),
         "store": 11,
-        "dateTime": "Data da compra",
-        "clientPosition": "Posição do cliente",
+        "dateTime": fake.iso8601(),
+        "clientPosition": fake.location_on_land(),
     }
