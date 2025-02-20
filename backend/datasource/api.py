4256d9206701a1f02c01239c768440dd9dd8f5c5
Douglas Saturnino <95532957+douglasaturnino@users.noreply.github.com>
2025-02-20 11:18:04 -0300

    adiciona api para coletar dados de compras (#1)
    * refactor: altera o nome das pastas back-end e fast-api para backend e fastapi
    
    * feat: adiciona classe APICollector para coletar dados de compras

---
+++ backend/datasource/api.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class APICollector:
+    def __init__(self):
+        self._schema = None
+        self._aws = None
+        self._buffer = None
+        return
+
+    def getData(self, param):
+        response = None
+        if param > 1:
+            response = requests.get(
+                f"http://127.0.0.01:8000/gerar_compras/{param}"
+            ).json()
+        else:
+            response = requests.get(
+                "http://127.0.0.01:8000/gerar_compra"
+            ).json()
+        return response
