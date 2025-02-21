2c831d66b951becb3c79fd75eb3d00fdeeb1ec86
Douglas Saturnino <douglasaturnino@gmail.com>
2025-02-21 14:56:17 -0300

    feat: adiciona função para extrair dados no coletor CSV

--- frontend/datasource/csv.py
+++ frontend/datasource/csv.py
@@ -10,3 +10,17 @@ class CSVCollector:
             "Insira o arquivo Excel", type=("csv", "xlsx")
         )
         return dados_excel
+
+    def extractData(self, dados_excel):
+        workbook = openpyxl.load_workbook(dados_excel)
+        sheet = workbook.active
+        range_cell = sheet[self.cell_range]
+
+        headers = [cell.value for cell in range_cell[0]]
+
+        data = []
+        for row in range_cell[1:]:
+            data.append([cell.value for cell in row])
+
+        dataframe = pd.DataFrame(data, columns=headers)
+        return dataframe
