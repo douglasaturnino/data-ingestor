55bf0d7eb61806a5b89cc08a431a757e9d210865
Douglas Saturnino <95532957+douglasaturnino@users.noreply.github.com>
2025-02-24 11:33:22 -0300

    feat: adiciona validação de dados no coletor CSV (#13)

--- frontend/datasource/csv.py
+++ frontend/datasource/csv.py
@@ -1,6 +1,7 @@
 import openpyxl
 import pandas as pd
 import streamlit as st
+from pydantic import ValidationError
 
 
 class CSVCollector:
@@ -12,6 +13,7 @@ class CSVCollector:
 
     def start(self):
         getData = self.getData()
+        extractData = None
 
         if getData is not None:
             extractData = self.extractData(getData)
@@ -37,4 +39,24 @@ class CSVCollector:
             data.append([cell.value for cell in row])
 
         dataframe = pd.DataFrame(data, columns=headers)
+        return dataframe
+
+    def validateData(self, dataframe):
+        error = []
+        valid_rows = []  # To store valid rows
+
+        for index, row in dataframe.iterrows():
+            try:
+                # Create an instance of the Pydantic model for each row
+                valid_row = self._schema(**row.to_dict())
+                valid_rows.append(valid_row)  # Add the valid row to the list
+            except ValidationError as e:
+                # Append error message for rows that fail validation
+                error.append(f"Erro na linha {index + 1}: {str(e)}")
+
+        if error:
+            st.error("\n".join(error))  # Displaying errors in Streamlit
+            return None  # Return None if there are errors
+
+        st.success("Tudo certo!")
         return dataframe
