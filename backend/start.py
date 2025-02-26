c39ca31b31ed15e191d486b19b1d4ea560ee97f1
Douglas Saturnino <douglasaturnino@gmail.com>
2025-02-26 13:39:07 -0300

    feat: adiciona função para coletar dados do PostgreSQL

--- backend/start.py
+++ backend/start.py
@@ -1,10 +1,17 @@
 from aws.cliente import S3Client
 from contracts.schema import compraShema
 from datasource.api import APICollector
+from datasource.postgre import PostgresCollector
 
 schema = compraShema
 aws = S3Client()
 
 minha_classe = APICollector(schema, aws).start(1)
 
-print(minha_classe)
+
+def getPostgre(aws, dbId):
+    postgres = PostgresCollector(aws, dbId).start()
+
+
+getPostgre(aws, dbId=1)
+getPostgre(aws, dbId=2)
